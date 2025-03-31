da4a2e5ab9164bf37e5a9a5e25018081df76e823
gastonfun <32093380+gastonfun@users.noreply.github.com>
2025-03-31 19:14:51 -0300

    asdqwed

--- script.js
+++ script.js
@@ -4,34 +4,13 @@ function toggleMenu() {
     menu.classList.toggle('oculto');
 }
 
-// Animacion de texto
-function animate({timing, draw, duration}) {
-
-    let start = performance.now();
-  
-    requestAnimationFrame(function animate(time) {
-      // timeFraction va de 0 a 1
-      let timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
-  
-      // calcular el estado actual de la animación
-      let progress = timing(timeFraction);
-  
-      draw(progress); // dibujar
-  
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
-      }
-  
-    });
-}
 
 // Contador regresivo
-const fechaBoda = new Date("2025-04-01T22:00:00").getTime();
+const fechaCumple = new Date("2025-04-01T22:00:00").getTime();
 
 const cuentaRegresiva = setInterval(() => {
     const ahora = new Date().getTime();
-    const distancia = fechaBoda - ahora;
+    const distancia = fechaCumple - ahora;
 
     const dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
     const horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
